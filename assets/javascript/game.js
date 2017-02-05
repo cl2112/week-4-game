f8ee49bc028cf8abe731841c57618a8628bacd93
cl2112 <cl2112cl2112@gmail.com>
2017-02-05 12:36:35 -0800

    added images and started basic layout of index.html and defined the variables for the characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+
+var loneStar {
+	atkPwr: 0,
+	counterAtk: 0,
+	health: 0,
+}
+
+var barf {
+	atkPwr: 0,
+	counterAtk: 0,
+	health: 0,
+}
+
+var darkHelmet {
+	atkPwr: 0,
+	counterAtk: 0,
+	health: 0,
+}
+
+var president {
+	atkPwr: 0,
+	counterAtk: 0,
+	health: 0,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
